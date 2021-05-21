08e087a9deacd6b731cfbe2c9cd639629ab1d7f4
Devvrat Chaubal <84012086+AccountantCoder123@users.noreply.github.com>
2021-05-21 12:02:06 +0530

    Update Derivative of Polynomial.py

--- Derivative of Polynomial.py
+++ Derivative of Polynomial.py
@@ -1,27 +1,25 @@
-import org.jetbrains.annotations.NotNull;
-
 import java.util.*;
 class Calculus {
-    public String Derivative(@NotNull List<Float> coef, List<Float> ind){
+    public String Derivative(List<Float> coef,List<Float> ind){
         int i=0;
-        StringBuilder s= new StringBuilder();
+        String s="";
         List<String> comb=new ArrayList<>();
         List<Float> f1=new ArrayList<>();
         List<Float> f2=new ArrayList<>();
         while(i<coef.size()){
             f1.add(coef.get(i)*ind.get(i));
             f2.add(ind.get(i)-1);
-            comb.add(Float.toString(f1.get(i))+'x'+'^'+ f2.get(i) +'+');
+            comb.add(Float.toString(f1.get(i))+'x'+'^'+Float.toString(f2.get(i))+'+');
             i++;
         }
-        String h;
-        h=comb.get(comb.size()-1).substring(0,comb.get(comb.size()-1).length()-1);
+        String h="";
         int j=0;
         while(j<comb.size()){
-            s.append(comb.get(j));
+            s=s+comb.get(j);
             j++;
         }
-        return s+h;
+        
+        return s.substring(0,s.length()-1);
     }
 }
 
